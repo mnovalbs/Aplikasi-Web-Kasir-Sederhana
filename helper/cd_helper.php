0878ab277937f436badac928010b1af54bfcf678
mnovalbs <masbintangblog@gmail.com>
2016-12-03 21:10:29 +0700

    Update

---
+++ helper/cd_helper.php
@@ -0,0 +1,9 @@
+<?php
+
+  function base_url($str = '')
+  {
+    $root_url = 'http://tubes.dev/';
+    return $root_url.$str;
+  }
+
+?>
--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
+  require_once('config/route.php');
+  require_once('helper/cd_helper.php');
   require_once('core/CD_Config.php');
   require_once('core/CD_Controller.php');
   require_once('core/CD_Loader.php');
   require_once('core/CD_Database.php');
   require_once('core/CD_Model.php');
-  require_once('config/route.php');
 ?>
