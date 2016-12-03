249740da0581e1430a9f1305b5a15f69d4300d77
mnovalbs <masbintangblog@gmail.com>
2016-12-03 21:29:36 +0700

    Update

--- view/petugas/petugas_login.php
+++ view/petugas/petugas_login.php
@@ -2,7 +2,7 @@
   $this->view('header');
 ?>
 
-  <form method='POST'>
+  <form method='POST' action='<?php echo base_url('petugas/login'); ?>'>
     <input name='email'/>
     <input name='password'/>
     <button type='submit' name='login'>Login</button>
